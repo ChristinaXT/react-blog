06299b92c70d94ef3a7d1f35a19cd0392a18dd75
Christina Williams <christinaxtwilliams@gmail.com>
2021-03-04 17:45:34 -0500

    display

--- src/Components/DisplayAllPosts.jsx
+++ src/Components/DisplayAllPosts.jsx
@@ -1,9 +1,22 @@
-import React from 'React';
+import React, {useState} from 'React';
 import CreateNewPost from './CreateNewPost'
 const DisplayAllPosts = () => {
+const [title, setTitle] = useState("");
+const [content, setContent] = useState("");
+const savePostTitleToState = event => {
+  setTitle(event.target.value);
+  console.log(title)
+};
+const savePostContentToState = event => {
+  setContent(event.target.value);
+  console.log(content)
+};
     return (
     <>
-    <CreateNewPost />
+    <CreateNewPost
+    savePostTitleToState = {savePostTitleToState}
+    savePostContentToState = {savePostContentToState}
+    />
     </>
     )
 }
