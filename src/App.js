d271e41a88489d487cb674133f061446327b1ba4
Christina Williams <christinaxtwilliams@gmail.com>
2021-03-04 17:42:27 -0500

    update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-
-import CreateNewPost from './Components/CreateNewPost'
+import Posts from './Components/Post'
 const App = ( ) => {
   return (
-    <div>
-      < CreateNewPost />
-    </div>
+    <>
+      <Posts />
+    </>
   );
 };
 export default App;
